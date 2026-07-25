1e8041df932d7c740ebf85ebfaa43edecf1cbf2b
mateuhnferreira-ai <mateuhnferreira@gmail.com>
2026-07-25 15:49:28 -0300

    Update motor-planejamento.js

--- docs/guia-estudos/motor-planejamento.js
+++ docs/guia-estudos/motor-planejamento.js
@@ -1,1 +1,62 @@
+// =====================================
+// MOTOR DE PLANEJAMENTO PSCPP
+// =====================================
 
+
+function gerarPlanoEstudo(){
+
+
+let plano = [];
+
+
+
+for(let disciplina in conteudoPSCPP){
+
+
+    let assuntos = conteudoPSCPP[disciplina].assuntos;
+
+
+
+    assuntos.forEach(assunto => {
+
+
+
+        if(assunto.status !== "Concluído"){
+
+
+            plano.push({
+
+                disciplina:
+                conteudoPSCPP[disciplina].nome,
+
+
+                assunto:
+                assunto.nome,
+
+
+                horas:
+                assunto.horas,
+
+
+                importancia:
+                assunto.importancia
+
+
+            });
+
+
+        }
+
+
+    });
+
+
+
+}
+
+
+
+return plano;
+
+
+}
