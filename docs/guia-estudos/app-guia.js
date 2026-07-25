044b65aa7f8b3ccd9ccabb43bff126bb19817eb4
mateuhnferreira-ai <mateuhnferreira@gmail.com>
2026-07-25 15:34:25 -0300

    Update app-guia.js

--- docs/guia-estudos/app-guia.js
+++ docs/guia-estudos/app-guia.js
@@ -1,1 +1,47 @@
+// =====================================
+// CONTROLE DO GUIA DE ESTUDOS PSCPP
+// =====================================
 
+
+console.log("Guia de Estudos carregado");
+
+
+
+let totalDisciplinas = Object.keys(conteudoPSCPP).length;
+
+
+let totalAssuntos = 0;
+
+
+let totalHoras = 0;
+
+
+
+for (let disciplina in conteudoPSCPP) {
+
+
+    let assuntos = conteudoPSCPP[disciplina].assuntos;
+
+
+    totalAssuntos += assuntos.length;
+
+
+
+    assuntos.forEach(assunto => {
+
+
+        totalHoras += assunto.horas;
+
+
+    });
+
+
+}
+
+
+
+console.log("Disciplinas:", totalDisciplinas);
+
+console.log("Assuntos:", totalAssuntos);
+
+console.log("Horas previstas:", totalHoras);
