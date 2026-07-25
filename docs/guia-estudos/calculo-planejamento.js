670dd5d6de8bd650e410f52b44e04a4fa99436dc
mateuhnferreira-ai <mateuhnferreira@gmail.com>
2026-07-25 16:03:06 -0300

    Update calculo-planejamento.js

--- docs/guia-estudos/calculo-planejamento.js
+++ docs/guia-estudos/calculo-planejamento.js
@@ -1,1 +1,53 @@
+// =====================================
+// CÁLCULO DO PLANEJAMENTO PSCPP
+// =====================================
 
+
+function calcularPlanejamento(){
+
+
+
+let inicio = new Date(configuracaoEstudo.dataInicio);
+
+
+let prova = new Date(configuracaoEstudo.dataProva);
+
+
+
+let diferenca = prova - inicio;
+
+
+
+let semanas = Math.floor(
+    diferenca / (1000 * 60 * 60 * 24 * 7)
+);
+
+
+
+let horasSemana =
+configuracaoEstudo.horasPorDia *
+configuracaoEstudo.diasPorSemana;
+
+
+
+let horasTotais =
+semanas * horasSemana;
+
+
+
+return {
+
+
+semanasDisponiveis: semanas,
+
+
+horasPorSemana: horasSemana,
+
+
+horasTotais: horasTotais
+
+
+};
+
+
+}
