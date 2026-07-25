a6809e55f7bcd9282a064593c23fbf3dbe8ed53e
mateuhnferreira-ai <mateuhnferreira@gmail.com>
2026-07-25 15:52:30 -0300

    Update app-planejamento.js

--- docs/guia-estudos/app-planejamento.js
+++ docs/guia-estudos/app-planejamento.js
@@ -1,1 +1,51 @@
+// =====================================
+// INTERFACE DO PLANEJAMENTO
+// =====================================
 
+
+let plano = gerarPlanoEstudo();
+
+
+
+let area = document.getElementById("lista-estudos");
+
+
+
+plano.forEach(item => {
+
+
+
+area.innerHTML += `
+
+
+<div class="card">
+
+
+<h3>
+${item.disciplina}
+</h3>
+
+
+<p>
+📖 ${item.assunto}
+</p>
+
+
+<p>
+⏱ ${item.horas} horas
+</p>
+
+
+<p>
+⭐ Prioridade: ${item.importancia}
+</p>
+
+
+</div>
+
+
+`;
+
+
+
+});
